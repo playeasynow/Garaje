7a59d65fffe3e2f33cc635a59536e5ee23e79465
Play Easy Now <16202860+playeasynow@users.noreply.github.com>
2019-02-07 10:18:47 -0600

    added menu description, started learning page

--- garaje-app/src/components/Carousel/index.js
+++ garaje-app/src/components/Carousel/index.js
@@ -14,7 +14,7 @@ function Carousel() {
                     <div className="carousel-caption" id="caption-1">
                         <h1 className="caption-shadow">new coffee shop</h1>
                         <h1 className="caption-shadow">hondu-colombiano eats</h1>
-                        <h1 className="caption-shadow">community learning space</h1>
+                        <h1 className="caption-shadow">community learning platform</h1>
                         <h1 className="welcome-garaje"><i><b>welcome to garaje</b></i></h1>
                         <img src={GarajeIcon} className="garaje-icon" alt="main space" />
                     </div>
@@ -23,9 +23,6 @@ function Carousel() {
                     <img src={GarajePic3} className="d-block w-100 carousel-pics" alt="people hanging out" />
                     <div className="carousel-caption" id="caption-2">
                         <h1 className="welcome-garaje"><i><b>coffee and food</b></i></h1>
-                        {/* <h1 className="caption-shadow">new coffee shop</h1>
-                        <h1 className="caption-shadow">hondu-colombiano eats</h1>
-                        <h1 className="caption-shadow">community learning space</h1> */}
                         <div className="light-menu mt-3">
                             <div className="row">
 
@@ -35,19 +32,19 @@ function Carousel() {
                                             <div className="d-flex w-100 justify-content-between">
                                                 <h5 className="mb-1 heading">Cafe con Leche</h5>
                                             </div>
-                                            <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                                            <p className="mb-1">Equal parts hot coffee, and hot or steamed milk. Make it the Latin variation and use condensed milk!</p>
                                         </a>
                                         <a href="#" className="list-group-item list-group-item-action menu-text default">
                                             <div className="d-flex w-100 justify-content-between">
                                                 <h5 className="mb-1 heading">Horchata Cold Brew</h5>
                                             </div>
-                                            <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                                            <p className="mb-1">Not your ordinary cold brew. We add our homemade horchata recipe into the mix, and top it off with cinnamon.</p>
                                         </a>
                                         <a href="#" className="list-group-item list-group-item-action menu-text default">
                                             <div className="d-flex w-100 justify-content-between">
                                                 <h5 className="mb-1 heading">Chocolate Santafereño</h5>
                                             </div>
-                                            <p className="mb-1">This steamy concoction features a salty, melted surprise at the bottom of the mug: white cheese.</p>
+                                            <p className="mb-1">This steamy hot chocolate concoction features a savory, melted surprise at the bottom of the mug: white cheese.</p>
                                         </a>
                                     </div>
                                 </div>
@@ -58,19 +55,19 @@ function Carousel() {
                                             <div className="d-flex w-100 justify-content-between">
                                                 <h5 className="mb-1 heading">Avocado Tostones</h5>
                                             </div>
-                                            <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                                            <p className="mb-1">Tostones are twice-fried plantain slices. Pair that savory crunch with the smoothness of a ripe avocado. </p>
                                         </a>
                                         <a href="#" className="list-group-item list-group-item-action menu-text default">
                                             <div className="d-flex w-100 justify-content-between">
                                                 <h5 className="mb-1 heading">Fried Yucca Pops + Crema Fresca</h5>
                                             </div>
-                                            <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                                            <p className="mb-1">Yuca, a.k.a. cassava root, air-fried cripsy and delicious. Dip the bite-sized potato-substitue into our fresh cream sauce. </p>
                                         </a>
                                         <a href="#" className="list-group-item list-group-item-action menu-text default">
                                             <div className="d-flex w-100 justify-content-between">
                                                 <h5 className="mb-1 heading">Guacamole + Plaintain Chips</h5>
                                             </div>
-                                            <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                                            <p className="mb-1">Look, we make the best guacamole. It has BANANAS in it. We dare you to eat it with our banana chips (pictured). We're not kidding.</p>
                                         </a>
                                     </div>
                                 </div>
@@ -82,19 +79,19 @@ function Carousel() {
                 <div className="carousel-item" data-interval="6000">
                     <img src={GarajePic2} className="d-block w-100 carousel-pics" alt="main space" />
                     <div className="carousel-caption" id="caption-3">
-                        <h1 className="welcome-garaje"><i><b>community space</b></i></h1>
-                        <h1 className="caption-shadow">new coffee shop</h1>
-                        <h1 className="caption-shadow">hondu-colombiano eats</h1>
-                        <h1 className="caption-shadow">community learning space</h1>
+                        <h1 className="welcome-garaje"><i><b>community learning</b></i></h1>
+                        <h3 className="caption-shadow">garaje's mission is to empower people to learn, grow and thrive</h3>
+                        <h3 className="caption-shadow">we want you to work on your personal projects with us</h3>
+                        <h3 className="caption-shadow">that's why we offer online video courses in web development, <br></br>creative writing, public speaking and music composition, with more to come.</h3>
                     </div>
                 </div>
             </div>
             <a className="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                {/* <span className="carousel-control-prev-icon" aria-hidden="true"></span> */}
                 <span className="sr-only">Previous</span>
             </a>
             <a className="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                {/* <span className="carousel-control-next-icon" aria-hidden="true"></span> */}
                 <span className="sr-only">Next</span>
             </a>
         </div>
