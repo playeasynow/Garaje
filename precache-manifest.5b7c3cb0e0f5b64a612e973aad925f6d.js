1d4bb4d64120365ad0c0a063006cb0ddf8b84ba8
Play Easy Now <16202860+playeasynow@users.noreply.github.com>
2019-02-07 11:16:28 -0600

    Updates

---
+++ precache-manifest.5b7c3cb0e0f5b64a612e973aad925f6d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ac7b32a27b2f3b152ad",
+    "url": "/Garaje/static/css/main.296d806d.chunk.css"
+  },
+  {
+    "revision": "0ac7b32a27b2f3b152ad",
+    "url": "/Garaje/static/js/main.0ac7b32a.chunk.js"
+  },
+  {
+    "revision": "313220969b35f4541944",
+    "url": "/Garaje/static/js/1.31322096.chunk.js"
+  },
+  {
+    "revision": "669120b7bad201e3652f",
+    "url": "/Garaje/static/js/runtime~main.669120b7.js"
+  },
+  {
+    "revision": "18248726f7bf6f53ca8f3a6006b80f10",
+    "url": "/Garaje/static/media/garaje-1.18248726.png"
+  },
+  {
+    "revision": "99b8bd7cd08d87cd317d2961a74c0863",
+    "url": "/Garaje/static/media/garaje-3.99b8bd7c.png"
+  },
+  {
+    "revision": "4bf70ddd2e6429c22c8b36c5f355e809",
+    "url": "/Garaje/static/media/garaje-5.4bf70ddd.png"
+  },
+  {
+    "revision": "b1fdd213e206b70c147251514ff29fd7",
+    "url": "/Garaje/static/media/garaje-logo.b1fdd213.png"
+  },
+  {
+    "revision": "8cf297197c3e052346b3200a5fee583d",
+    "url": "/Garaje/static/media/yay-match.8cf29719.gif"
+  },
+  {
+    "revision": "e017f3ce78ebe24f047de839c54b9d80",
+    "url": "/Garaje/index.html"
+  }
+];
