2f87a0e10ee87375ed23e1a18a7cc4ec752a9e12
Play Easy Now <16202860+playeasynow@users.noreply.github.com>
2019-02-06 21:47:33 -0600

    added menu

--- garaje-app/src/components/Carousel/index.js
+++ garaje-app/src/components/Carousel/index.js
@@ -2,33 +2,90 @@ import React from "react";
 import GarajePic1 from "../../images/garaje-1.png"
 import GarajePic2 from "../../images/garaje-3.png"
 import GarajePic3 from "../../images/garaje-5.png"
+import GarajeIcon from "../../images/garaje-logo.png"
 import "./style.css";
 
 function Carousel() {
     return (
         <div id="carouselExampleInterval" className="carousel slide" data-ride="carousel">
             <div className="carousel-inner">
-                <div className="carousel-item active" data-interval="10000">
+                <div className="carousel-item active" data-interval="6000">
                     <img src={GarajePic1} className="d-block w-100 carousel-pics" alt="main space" />
                     <div className="carousel-caption" id="caption-1">
                         <h1 className="caption-shadow">new coffee shop</h1>
                         <h1 className="caption-shadow">hondu-colombiano eats</h1>
                         <h1 className="caption-shadow">community learning space</h1>
-                        <h1 className="welcome-garaje">welcome to garaje</h1>
+                        <h1 className="welcome-garaje"><i><b>welcome to garaje</b></i></h1>
+                        <img src={GarajeIcon} className="garaje-icon" alt="main space" />
                     </div>
                 </div>
-                <div className="carousel-item" data-interval="10000">
+                <div className="carousel-item" data-interval="6000">
                     <img src={GarajePic3} className="d-block w-100 carousel-pics" alt="people hanging out" />
                     <div className="carousel-caption" id="caption-2">
-                        <h5>HELLO</h5>
-                        <p>how's it going?</p>
+                        <h1 className="welcome-garaje"><i><b>coffee and food</b></i></h1>
+                        {/* <h1 className="caption-shadow">new coffee shop</h1>
+                        <h1 className="caption-shadow">hondu-colombiano eats</h1>
+                        <h1 className="caption-shadow">community learning space</h1> */}
+                        <div className="light-menu mt-3">
+                            <div className="row">
+
+                                <div className="col">
+                                    <div className="list-group menu-bg">
+                                        <a href="#" className="list-group-item list-group-item-action menu-text default">
+                                            <div className="d-flex w-100 justify-content-between">
+                                                <h5 className="mb-1 heading">Cafe con Leche</h5>
+                                            </div>
+                                            <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                                        </a>
+                                        <a href="#" className="list-group-item list-group-item-action menu-text default">
+                                            <div className="d-flex w-100 justify-content-between">
+                                                <h5 className="mb-1 heading">Horchata Cold Brew</h5>
+                                            </div>
+                                            <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                                        </a>
+                                        <a href="#" className="list-group-item list-group-item-action menu-text default">
+                                            <div className="d-flex w-100 justify-content-between">
+                                                <h5 className="mb-1 heading">Chocolate Santafereño</h5>
+                                            </div>
+                                            <p className="mb-1">This steamy concoction features a salty, melted surprise at the bottom of the mug: white cheese.</p>
+                                        </a>
+                                    </div>
+                                </div>
+
+                                <div className="col">
+                                    <div className="list-group menu-bg">
+                                        <a href="#" className="list-group-item list-group-item-action menu-text default">
+                                            <div className="d-flex w-100 justify-content-between">
+                                                <h5 className="mb-1 heading">Avocado Tostones</h5>
+                                            </div>
+                                            <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                                        </a>
+                                        <a href="#" className="list-group-item list-group-item-action menu-text default">
+                                            <div className="d-flex w-100 justify-content-between">
+                                                <h5 className="mb-1 heading">Fried Yucca Pops + Crema Fresca</h5>
+                                            </div>
+                                            <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                                        </a>
+                                        <a href="#" className="list-group-item list-group-item-action menu-text default">
+                                            <div className="d-flex w-100 justify-content-between">
+                                                <h5 className="mb-1 heading">Guacamole + Plaintain Chips</h5>
+                                            </div>
+                                            <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                                        </a>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="carousel-item" data-interval="10000">
+                <div className="carousel-item" data-interval="6000">
                     <img src={GarajePic2} className="d-block w-100 carousel-pics" alt="main space" />
                     <div className="carousel-caption" id="caption-3">
-                        <h5>HELLO</h5>
-                        <p>how's it going?</p>
+                        <h1 className="welcome-garaje"><i><b>community space</b></i></h1>
+                        <h1 className="caption-shadow">new coffee shop</h1>
+                        <h1 className="caption-shadow">hondu-colombiano eats</h1>
+                        <h1 className="caption-shadow">community learning space</h1>
                     </div>
                 </div>
             </div>
